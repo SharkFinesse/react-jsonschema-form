1fdbc7c08565a04caaaf73d5dc47505752543f44
Robert Jones <b.jones@sharkfinesse.com>
2020-12-09 14:43:16 +0000

    build

--- packages/antd/dist/es/components/DatePicker/index.js
+++
@@ -1,4 +0,0 @@
-import dayjsGenerateConfig from 'rc-picker/lib/generate/dayjs';
-import generatePicker from 'antd/lib/date-picker/generatePicker';
-var DatePicker = generatePicker(dayjsGenerateConfig);
-export default DatePicker;
--- packages/material-ui/dist/FieldTemplate/FieldTemplate.d.ts
+++
@@ -1,3 +0,0 @@
-import { FieldTemplateProps } from "@rjsf/core";
-declare const FieldTemplate: ({ id, children, classNames, disabled, displayLabel, label, onDropPropertyClick, onKeyChange, readonly, required, rawErrors, rawHelp, rawDescription, schema, }: FieldTemplateProps) => JSX.Element;
-export default FieldTemplate;
